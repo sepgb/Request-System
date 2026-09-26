616bec97113869b78302e93e506d1f5843fea234
sepgb <josephgobres06@gmail.com>
2026-09-26 19:40:45 +0800

    fixed admin dropdown header visibility in light mode

--- admin/login.php
+++ admin/login.php
@@ -1,7 +1,7 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-
+/*
+Registration key: URS-REGISTRAR-2026
+*/
 session_start();
 require_once '../config/db.php';
 require_once '../includes/functions.php';
